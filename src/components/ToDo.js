89db9a8f19931f092bf7e5f0f2fe9deed71a6397
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-06-27 12:39:06 +0200

    rendering list of tasks

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,14 +2,30 @@ import React from "react";
 
 export class ToDo extends React.Component {
     state = {
-        task: 'TO DO'
+        tasks: [
+        {
+            id: 123,
+            text: 'Kupić prezenty dla PKO',
+            isCompleted: false
+        },
+        {
+            id: 321,
+            text: 'Kupić kanapy do salonu',
+            isCompleted: false
+        }
+    ]
     }
 
     render() {
+        const { tasks } = this.state;
+        
         return (
             <div>
                 <ul>
-                    { this.state.task }
+                    { this.state.tasks.map(({id, text}) => {
+                        return <li key={id}>{task.text}</li>
+                    })
+                    }
                 </ul>
             </div>
         )
