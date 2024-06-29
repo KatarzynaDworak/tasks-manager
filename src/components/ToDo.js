2c2f0b1aae7d4ada9ed5810a02591fd29f1e85c1
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-06-29 13:38:03 +0200

    form & addNewTask method added

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,31 +2,68 @@ import React from "react";
 
 export class ToDo extends React.Component {
     state = {
+        newTaskValue: '',
         tasks: [
         {
             id: 123,
-            text: 'Kupić prezenty dla PKO',
+            text: 'Odebrać alkohole dla PKO',
             isCompleted: false
         },
         {
             id: 321,
-            text: 'Kupić kanapy do salonu',
+            text: 'Znaleźć kanapy do salonu',
             isCompleted: false
         }
     ]
     }
 
+    onNewTasValueChange = (e) => {
+        this.setState((prevState) => ({
+            ...prevState,
+            newTaskValue: e.target.value
+        }))
+    }
+
+    addNewTask = (e) => {
+        e.preventDefault();
+
+        if(!this.state.newTaskValue) return
+
+        const newTask = {
+            id: Date.now(),
+            text: this.state.newTaskValue,
+            isCompleted: false
+        }
+
+        this.setState((prevState) => ({
+            newTaskValue: '',
+            tasks: prevState.tasks.concat(newTask)
+        }))
+    }
+    
+
     render() {
-        const { tasks } = this.state;
-        
+        const { tasks, newTaskValue } = this.state;
+
         return (
             <div>
-                <ul>
-                    { this.state.tasks.map(({id, text}) => {
-                        return <li key={id}>{task.text}</li>
-                    })
-                    }
-                </ul>
+                <form
+                onSubmit={this.addNewTask}>
+                    <input 
+                        name="text"
+                        value={newTaskValue}
+                        onChange={this.onNewTasValueChange}
+                    >
+                    </input>
+                    <br></br>
+                    <button>SUBMIT</button>
+                    <ul>
+                        { tasks.map(({id, text}) => {
+                            return <li key={id}>{text}</li>
+                        })
+                        }
+                    </ul>
+                </form>
             </div>
         )
     }
